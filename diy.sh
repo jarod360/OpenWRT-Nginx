d14034deb94cdbaf3f9dec0b5074cd9a47bbf905
jarod360 <32123167+jarod360@users.noreply.github.com>
2020-06-04 23:51:07 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,9 +2,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.0.250/g' package/base-files/files/bin/config_generate
 
-#加载个性化配置
-cp -f ../default-settings package/lean/default-settings/files/zzz-default-settings
-
 #移除不用软件包    
 rm -rf package/lean/luci-app-dockerman
 rm -rf package/lean/luci-theme-argon
@@ -42,6 +39,9 @@ git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon pack
 git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+#加载个性化配置
+cp -f ../default-settings package/lean/default-settings/files/zzz-default-settings
 
 #修改版本号
 sed -i 's/OpenWrt/JarodChang Build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
