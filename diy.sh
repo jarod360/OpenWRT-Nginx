36fe3fe1a61988813f6251a969f485bd96667cd7
jarod360 <32123167+jarod360@users.noreply.github.com>
2020-06-16 02:31:25 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -10,7 +10,7 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-ser
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
 git clone https://github.com/Repobor/luci-app-koolproxyR.git package/luci-app-koolproxyR
 svn co https://github.com/Lienol/openwrt-package/trunk/package/tcping package/tcping
-git clone --depth=1 https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
+git clone https://github.com/pexcn/openwrt-chinadns-ng.git package/chinadns-ng
 svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus package/luci-app-ssr-plus
 svn co https://github.com/Lienol/openwrt-package/trunk/package/brook
 svn co https://github.com/Lienol/openwrt-package/trunk/package/ipt2socks
@@ -26,16 +26,14 @@ git clone https://github.com/brvphoenix/wrtbwmon package/wrtbwmon
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/pymumu/luci-app-smartdns -b lede package/luci-app-smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-# Add OpenClash
-git clone --depth=1 https://github.com/vernesong/OpenClash package/OpenClash
 # Add Lienol's Packages
-git clone --depth=1 https://github.com/SuLingGG/openwrt-package package/Lienol
+git clone https://github.com/SuLingGG/openwrt-package package/Lienol
 # Add Rclone-OpenWrt
 git clone https://github.com/ElonH/Rclone-OpenWrt package/Rclone-OpenWrt
 # Add luci-theme-atmaterial-ci
 git clone https://github.com/esirplayground/luci-theme-atmaterial-ColorIcon package/luci-theme-atmaterial-ColorIcon
 # Add luci-theme-argon
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 
 #加载个性化配置
 cp -f ../default-settings package/lean/default-settings/files/zzz-default-settings
