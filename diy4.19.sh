7fbaec32f9e48cff45aadd637730841f5ec3d592
jarod360 <32123167+jarod360@users.noreply.github.com>
2020-12-10 12:47:30 +0800

    Update diy4.19.sh

--- diy4.19.sh
+++ diy4.19.sh
@@ -16,7 +16,9 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 #移除不用软件包    
 rm -rf package/lean/luci-app-dockerman
 rm -rf package/lean/luci-theme-argon
+rm -rf feeds/packages/net/https-dns-proxy
 #添加额外软件包
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy package/https-dns-proxy
 svn co https://github.com/siropboy/mypackages/trunk/smartdns package/smartdns
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-smartdns package/luci-app-smartdns
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-koolproxyR package/luci-app-koolproxyR
