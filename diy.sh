a2a5349c212d48fa488470868296bf502879a158
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-01-25 11:08:17 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -30,7 +30,7 @@ git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos
 git clone https://github.com/garypang13/luci-app-baidupcs-web package/luci-app-baidupcs-web
 git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
 git clone https://github.com/brvphoenix/wrtbwmon package/wrtbwmon
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
