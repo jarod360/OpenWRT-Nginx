0f76f36997c384acb2c9e3a801acec839186f734
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-03-29 16:57:35 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -20,7 +20,8 @@ svn co https://github.com/siropboy/mypackages/trunk/luci-app-koolproxyR package/
 #svn co https://github.com/openwrt/luci/trunk/applications/luci-app-sqm package/luci-app-sqm
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-control-timewol package/luci-app-control-timewol
-svn co https://github.com/sirpdboy/sirpdboy-package/trunk/adguardhome package/adguardhome
+svn co https://github.com/jarod360/packages/trunk/sub-web package/sub-web
+svn co https://github.com/garypang13/openwrt-packages/trunk/subconverter package/subconverter
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
