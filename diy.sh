9857589e4df25dca360f97fb3a33169434bdd9e5
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-05-10 01:45:21 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,8 +9,8 @@ sed -i 's/OpenWrt/OpenWrt Build '$date' By JarodChang/g' package/lean/default-se
 sed -i 's/%D %V, %C/%D %V, '$date' By JarodChang/g' package/base-files/files/etc/banner
 sed -i "28a\echo \'*/60 * * * * sh /etc/memclean.sh\' > /etc/crontabs/root" package/lean/default-settings/files/zzz-default-settings
 sed -i '56a\chmod 1777 /etc/memclean.sh' package/lean/default-settings/files/zzz-default-settings
-sed -i "248\		set system.@system[-1].conloglevel='1'" package/base-files/files/bin/config_generate
-sed -i "249\		set system.@system[-1].kconloglevel='1'" package/base-files/files/bin/config_generate
+sed -i "248\		set system.@system[-1].conloglevel=\'1\'" package/base-files/files/bin/config_generate
+sed -i "249\		set system.@system[-1].kconloglevel=\'1\'" package/base-files/files/bin/config_generate
 #设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 # 修改插件名字
