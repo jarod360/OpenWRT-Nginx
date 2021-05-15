ca26bb740e65d57594ed14388760df5150daff81
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-05-16 02:37:19 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,14 +18,11 @@ sed -i 's/"带宽监控"/"带宽"/g' feeds/luci/applications/luci-app-nlbwmon/po
 #移除不用软件包    
 rm -rf package/lean/luci-app-dockerman
 rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-ttyd
 #添加额外软件包
-svn co https://github.com/openwrt/luci/trunk/applications/luci-app-ttyd package/luci-app-ttyd
 svn co https://github.com/immortalwrt/packages/trunk/lang/node-yarn package/node-yarn
 svn co https://github.com/siropboy/mypackages/trunk/smartdns package/smartdns
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-smartdns package/luci-app-smartdns
 svn co https://github.com/Beginner-Go/my-packages/trunk/luci-app-koolproxyR  package/luci-app-koolproxyR
-svn co https://github.com/openwrt/luci/trunk/applications/luci-app-sqm package/luci-app-sqm
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-control-timewol package/luci-app-control-timewol
 svn co https://github.com/jarod360/packages/trunk/sub-web package/sub-web
