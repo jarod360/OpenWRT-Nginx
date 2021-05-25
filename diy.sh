26671cf57913f8543b151fc6ba39a388cf069efd
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-05-25 18:00:22 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -24,7 +24,7 @@ rm -rf feeds/packages/utils/ttyd
 svn co https://github.com/immortalwrt/packages/trunk/lang/node-yarn package/node-yarn
 svn co https://github.com/siropboy/mypackages/trunk/smartdns package/smartdns
 svn co https://github.com/siropboy/mypackages/trunk/luci-app-smartdns package/luci-app-smartdns
-svn co https://github.com/Beginner-Go/my-packages/trunk/luci-app-koolproxyR  package/luci-app-koolproxyR
+#svn co https://github.com/Beginner-Go/my-packages/trunk/luci-app-koolproxyR  package/luci-app-koolproxyR
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-advanced package/luci-app-advanced
 svn co https://github.com/siropboy/sirpdboy-package/trunk/luci-app-control-timewol package/luci-app-control-timewol
 svn co https://github.com/jarod360/packages/trunk/sub-web package/sub-web
@@ -34,6 +34,7 @@ svn co https://github.com/garypang13/openwrt-packages/trunk/quickjspp package/qu
 svn co https://github.com/garypang13/openwrt-packages/trunk/rapidjson package/rapidjson
 svn co https://github.com/jarod360/packages/trunk/subconverter package/subconverter
 svn co https://github.com/jarod360/packages/trunk/ttyd package/ttyd
+git clone https://github.com/godros/luci-app-godproxy.git package/luci-app-godproxy
 git clone https://github.com/jarod360/luci-app-ttyd package/luci-app-ttyd
 git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
