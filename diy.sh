d9a65e9e5d6f81435c3e0c4b3ca87ffea815a0b1
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-06-01 01:52:26 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -46,7 +46,7 @@ git clone https://github.com/garypang13/luci-app-eqos package/luci-app-eqos
 git clone https://github.com/garypang13/luci-app-baidupcs-web package/luci-app-baidupcs-web
 #git clone https://github.com/brvphoenix/luci-app-wrtbwmon package/luci-app-wrtbwmon
 #git clone https://github.com/brvphoenix/wrtbwmon package/wrtbwmon
-git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/fw876/helloworld.git package/helloworld
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
@@ -55,6 +55,7 @@ rm -rf package/passwall/v2ray-core
 rm -rf package/passwall/v2ray-plugin
 rm -rf package/passwall/xray-core
 rm -rf package/passwall/xray-plugin
+rm -rf package/passwall/naiveproxy
 
 # Add Rclone-OpenWrt
 git clone https://github.com/ElonH/Rclone-OpenWrt package/Rclone-OpenWrt
