924f9c3a7219e58f3db5f1ac47f19b890ff5615a
jarod360 <32123167+jarod360@users.noreply.github.com>
2021-06-22 20:16:34 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,6 +9,7 @@ sed -i 's/OpenWrt/OpenWrt Build '$date' By Jarod Chang/g' package/lean/default-s
 sed -i 's/%D %V, %C/%D %V, '$date' By Jarod Chang/g' package/base-files/files/etc/banner
 #加入定时清理内存
 sed -i "28a\echo \'*/60 * * * * sh /etc/memclean.sh\' > /etc/crontabs/root" package/lean/default-settings/files/zzz-default-settings
+sed -i '29a\nginx-util add_ssl _lan "zesun" "/etc/ssl/www.zesun.vip_chain.crt" "/etc/ssl/www.zesun.vip_key.key"' package/lean/default-settings/files/zzz-default-settings
 sed -i '56a\chmod 1777 /etc/memclean.sh' package/lean/default-settings/files/zzz-default-settings
 #设置密码为空（安装固件时无需密码登陆，然后自己修改想要的密码）
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
