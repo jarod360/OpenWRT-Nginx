101ebf6d2e26244497e809f27af524d32f306cd1
jarod360 <32123167+jarod360@users.noreply.github.com>
2022-05-03 20:16:07 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -20,9 +20,9 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 sed -i 's/"带宽监控"/"带宽"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
 #sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
 #移除不用软件包    
-rm -rf package/lean/luci-app-docker
-rm -rf package/lean/luci-theme-argon
-rm -rf package/lean/luci-app-ttyd
+rm -rf feeds/luci/applications/luci-app-docker
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-ttyd
 rm -rf feeds/packages/utils/ttyd
 rm -rf feeds/luci/collections/luci
 #添加额外软件包
