9831be69f825f3c13eec443afa80b028af0f096a
jarod360 <32123167+jarod360@users.noreply.github.com>
2022-05-29 13:43:03 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,9 @@ sed -i 's/192.168.1.1/192.168.0.250/g' package/base-files/files/bin/config_gener
 date=`date +%Y.%m.%d`
 sed -i 's/OpenWrt/OpenWrt Build '$date' By Jarod Chang/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/%D %V, %C/%D %V, '$date' By Jarod Chang/g' package/base-files/files/etc/banner
+#修改uwsgi超时时间
+sed -i '$a cgi-timeout = 600' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+sed -i '$a cgi-timeout = 600' feeds/packages/net/uwsgi/files-luci-support/luci-cgi_io.ini
 #加入定时清理内存
 #sed -i "28a\echo \'*/60 * * * * sh /etc/memclean.sh\' > /etc/crontabs/root" package/lean/default-settings/files/zzz-default-settings
 # 修改内核版本（版本内核默认5.15，还有5.4跟5.10内核）
