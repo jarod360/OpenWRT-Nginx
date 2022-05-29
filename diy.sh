0f1cd5948125a0d5c2619c834b18262a2931f866
jarod360 <32123167+jarod360@users.noreply.github.com>
2022-05-29 13:46:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -26,7 +26,6 @@ sed -i '59a\chmod 600 /root/.ssh/id_rsa.pub & chmod 600 /root/.ssh/id_rsa' packa
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 # 修改插件名字
 sed -i 's/"带宽监控"/"带宽"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
-#sed -i 's/"实时流量监测"/"流量"/g' package/lean/luci-app-wrtbwmon/po/zh-cn/wrtbwmon.po
 #移除不用软件包    
 rm -rf feeds/luci/applications/luci-app-docker
 rm -rf feeds/luci/themes/luci-theme-argon
