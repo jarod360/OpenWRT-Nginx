ede2ec2d4b21b8932e00be81ad648a38ce573004
jarod360 <32123167+jarod360@users.noreply.github.com>
2022-06-01 13:23:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,6 +7,8 @@ sed -i 's/192.168.1.1/192.168.0.250/g' package/base-files/files/bin/config_gener
 date=`date +%Y.%m.%d`
 sed -i 's/OpenWrt/OpenWrt Build '$date' By Jarod Chang/g' package/lean/default-settings/files/zzz-default-settings
 sed -i 's/%D %V, %C/%D %V, '$date' By Jarod Chang/g' package/base-files/files/etc/banner
+#修改luci显示cpu型号方式
+sed -i 's/pcdata(boardinfo.model/pcdata(boardinfo.system/g' feeds/luci/modules/luci-mod-admin-full/luasrc/view/admin_status/index.htm
 #修改GCC版本和binutils版本
 sed -i 's/default GCC_USE_VERSION_8/default GCC_USE_VERSION_11/g' toolchain/gcc/Config.in
 sed -i '22,23d' toolchain/gcc/Config.version
